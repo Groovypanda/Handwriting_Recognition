830cc4006e9ec457176edd2450832a8946b0af65
raddecke <ruben.dedecker@ugent.be>
2017-11-30 00:25:41 +0100

    word recognition and character extraction skeleton

--- tokenisation/word_extraction.py
+++ tokenisation/word_extraction.py
@@ -2,10 +2,18 @@
 
 import cv2
 import numpy as np
+import os
+import shutil
+dir = os.path.dirname(__file__)
+
 
 fileindex = 1;
-path = "/home/dexa/Ugent/AI/Project/OwnProject/"
-filepath = "/home/dexa/Ugent/AI/Project/OwnProject/OwnData/"
+outputpath = os.path.join(dir, '../data/output/')
+filepath = os.path.join(dir, '../data/texts/')
+
+# clear previous output
+shutil.rmtree(outputpath)
+os.makedirs(outputpath)
 
 # checks if rectangle 2 inside rectangle 1
 def rectangle_contains_rectangle(rectangle1, rectangle2):
@@ -15,21 +23,26 @@ def rectangle_contains_rectangle(rectangle1, rectangle2):
     else:
         return False
 
+y_overlap_divisor = 2
 # checks if rectangle 1 is left of rectangle right or just next to each other
-def rectangle_follows_rectangle(rectangle1, rectangle2):
+def rectangle_follows_rectangle(rectangle1, rectangle2, distance):
     ((x1, y1, w1, h1), (x2, y2, w2, h2)) = (rectangle1, rectangle2)
     # x1 left of x2
     # right side of rect1 less that 1 pixer away from left side rect2 (or inside rect 2)
-    if x1 <= x2 and (x1 + w1 - x2 >= -1):
+    if x1 <= x2 and (x1 + w1 - x2 >= distance):
         if y1 > y2 and y1 < y2 + h2:
             overlap = abs(y2 + h2 - y1)
-            if (overlap >= h1 / 2) or (overlap >= h2 / 2):
+            if (overlap >= h1 / y_overlap_divisor) or (overlap >= h2 / y_overlap_divisor):
                 return True;
         if y1 <= y2 and y2 < y1 + h1:
             overlap = abs(y1 + h1 - y2)
-            if (overlap >= h1 / 2) or (overlap >= h2 / 2):
+            if (overlap >= h1 / y_overlap_divisor) or (overlap >= h2 / y_overlap_divisor):
                 return True;
     return False
+
+def check_same_line (rectangle1, rectangle2, distance):
+    ((x1, y1, w1, h1), (x2, y2, w2, h2)) = (rectangle1, rectangle2)
+
 
 # rectangle 2 is right of rectangle 1
 def create_new_rectangle( rectangle1, contour1, rectangle2, contour2 ):
@@ -42,16 +55,12 @@ def create_new_rectangle( rectangle1, contour1, rectangle2, contour2 ):
     rectangle3 = (x3, y3, w3, h3)
 
     #TODO::
-    contour3 = 0
-
+    contour3 = np.concatenate((contour1, contour2))
     return ( rectangle3 , contour3 )
 
+for file in os.listdir(filepath):
+    print(file)
 
-
-for file in ( "text1.jpg","tekst2.png","text3.jpeg","text4.JPG","text5.JPG","text6.png"):
-
-
-    print(filepath+file)
     #PREPROCESSING
 
     # 1. reading image in greyscale
@@ -70,11 +79,16 @@ for file in ( "text1.jpg","tekst2.png","text3.jpeg","text4.JPG","text5.JPG","tex
 
     #cv2.imwrite(path + "words/text1.png", blur)
 
-    cv2.imwrite(path + "words/text" + str(fileindex) + ".png", thresh)
+    threshold_directory_path = os.path.join(dir, outputpath + 'thresholds')
+    thresholdpath = os.path.join(dir, outputpath + 'thresholds/image' + str(fileindex) + 'threshold.png')
+    if not os.path.exists(threshold_directory_path):
+        os.makedirs(threshold_directory_path)
+
+    cv2.imwrite(thresholdpath, thresh)
 
     # Finding contours
 
-    im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
 
     #skews = []
@@ -121,7 +135,7 @@ for file in ( "text1.jpg","tekst2.png","text3.jpeg","text4.JPG","text5.JPG","tex
                 for rectangle2 in rectangles_contours:
                     if not interrupted:
                         if rectangle1 != rectangle2:
-                            if rectangle_follows_rectangle(rectangle1, rectangle2):
+                            if rectangle_follows_rectangle(rectangle1, rectangle2, -1):
                                 (rectangle3, contour3) = create_new_rectangle(rectangle1, rectangles_contours[rectangle1], rectangle2, rectangles_contours[rectangle2])
                                 rectangles_to_remove.append(rectangle1)
                                 rectangles_to_remove.append(rectangle2)
@@ -171,6 +185,40 @@ for file in ( "text1.jpg","tekst2.png","text3.jpeg","text4.JPG","text5.JPG","tex
             for rectangle in rectangles_to_remove:
                 rectangles_contours.pop(rectangle, None)
 
+    # ordering the found words in lines
+    lines = list()
+    rectangles_copy = rectangles_contours.keys()
+    sorted_rectangles_copy = sorted(rectangles_copy, key=lambda x: (x[1], x[0]))
+    linesleft = len(rectangles_copy) != 0
+    iteration = 0;
+    while (linesleft):
+        highestrectangle = (0, 20000, 0, 0);
+
+        #search the highest remaining rectangle
+        for rectangle in rectangles_copy:
+            if (rectangle[1] < highestrectangle[1]):
+                highestrectangle = rectangle
+
+        # finding rest of the line
+        for rectangle in rectangles_copy:
+                if (rectangle[0] < highestrectangle[0] and rectangle_follows_rectangle(rectangle, highestrectangle):
+                        lines[iteration].append(rectangle)
+                if (rectangle[0] >= highestrectangle[0] and rectangle_follows_rectangle(highestrectangle, rectangle):
+                        lines[iteration].append(rectangle)
+        (x, y, w, h) = highestrectangle
+        xmin = x
+        ymin = y
+        xmax = x + w
+        ymax = y + h
+        highestrect = img[ymin:ymax, xmin:xmax]
+        cv2.imwrite(thresholdpath, highestrect)
+        #linesleft = len(rectangles_copy) != 0
+        linesleft = False
+
+
+
+
+
     # saving the found rectangles
     ind = 0;
     for rectangle in rectangles_contours:
@@ -181,10 +229,15 @@ for file in ( "text1.jpg","tekst2.png","text3.jpeg","text4.JPG","text5.JPG","tex
         ymax = y + h
         extracted_word = img[ymin:ymax, xmin:xmax]
 
-        cv2.imwrite(path + "words/text" + str(fileindex) + "/word" + str(ind) + ".png", extracted_word)
+        word_directory_path = os.path.join(dir, outputpath + 'text' + str(fileindex) + '/words/')
+        wordpath = os.path.join(dir, outputpath + 'text' + str(fileindex) + '/words/word' + str(ind) + '.png')
+        if not os.path.exists(word_directory_path):
+            os.makedirs(word_directory_path)
+
+        cv2.imwrite(wordpath, extracted_word)
         ind += 1
 
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
+        #cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
 
     finalcontours = list()
     for key in rectangles_contours:
@@ -194,7 +247,12 @@ for file in ( "text1.jpg","tekst2.png","text3.jpeg","text4.JPG","text5.JPG","tex
 
     # cv2.drawContours(img, finalcontours, -1, (0,255,0), 1)
 
-    cv2.imwrite(path + "parsed_texts/text" + str(fileindex) + ".png", img)
+    parsed_text_directory = os.path.join(dir, outputpath + 'parsed_texts/')
+    parsedtextpath = os.path.join(dir, outputpath + 'parsed_texts/text' + str(fileindex) + ".png")
+    if not os.path.exists(parsed_text_directory):
+        os.makedirs(parsed_text_directory)
+
+    cv2.imwrite(parsedtextpath, img)
 
     fileindex += 1
 
