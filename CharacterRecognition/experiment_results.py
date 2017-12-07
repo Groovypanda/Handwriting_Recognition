340c319988abd30c30538e4969805ff52a569443
Jarre Knockaert <Jarre.Knockaert@UGent.be>
2017-12-07 11:10:05 +0100

    moarrr verslag

--- CharacterRecognition/experiment_results.py
+++ CharacterRecognition/experiment_results.py
@@ -1,4 +1,5 @@
 import settings
+import math
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,19 +11,33 @@ experiments = ['base', 'batchsize', 'decay', 'filtersize', 'keepprob', 'learning
 ITERATIONS = 4
 
 
-def get_experiment_results(experiment_name):
-    confs, accuracy_avgs, time_avgs = [], [], []
+def get_experiment_results(experiment_name, max=True):
+    confs, accuracies, times = [], [], []
     for conf, dir in find_experiment_location(experiment_name):
-        accuracy = []
-        time = []
+        # Find the best result (result for which accuracy is highest in the end)
+        avg_accuracy = []
+        avg_time = []
+        max_accuracy = [0]
+        max_time = []
         for i in range(ITERATIONS):
             file_name = os.path.join(dir, '{}_' + str(i) + '.txt')
-            accuracy.append(np.loadtxt(file_name.format('accuracy')))
-            time.append(np.loadtxt(file_name.format('time')))
+            accuracy = np.loadtxt(file_name.format('accuracy'))
+            time = np.loadtxt(file_name.format('time'))
+            if max:
+                if accuracy[-1] > max_accuracy[-1]:
+                    max_accuracy = accuracy
+                    max_time = time
+            else:
+                avg_accuracy.append(accuracy)
+                avg_time.append(time)
         confs.append(conf)
-        accuracy_avgs.append(np.mean(accuracy, axis=0))
-        time_avgs.append(np.mean(time, axis=0))
-    return confs, accuracy_avgs, time_avgs
+        if max:
+            accuracies.append(max_accuracy)
+            times.append(max_time)
+        else:  # Calculate mean if not max
+            accuracies.append(np.mean(avg_accuracy, axis=0))
+            times.append(np.mean(avg_time, axis=0))
+    return confs, accuracies, times
 
 
 def get_configuration_results(experiment_name, conf):
@@ -46,6 +61,10 @@ def find_experiment_conf_location(experiment_name, conf):
 def visualise(accuracies, times, name='', show=True, save=False, labels=None):
     plots = []
     for i in range(len(accuracies)):
+        if labels[i]:
+            print("{}: {:.0f}%, {:.0f}".format(labels[i], 100 * accuracies[i][-1], times[i][-1]))
+        else:
+            print(accuracies[i][-1], times[i][-1])
         if labels is None:
             plot, = plt.plot(times[i], accuracies[i])
         else:
@@ -57,19 +76,22 @@ def visualise(accuracies, times, name='', show=True, save=False, labels=None):
     plt.xlabel("Time (s)")
     axes = plt.gca()
     axes.set_ylim([0, 0.9])
+    if save and name != '':
+        savefig(os.path.join(settings.EXPERIMENTS_CHAR_GRAPHS_PATH, name + '.png'))
     if show:
         plt.show()
-    if save and name != '' :
-        savefig(os.path.join(settings.EXPERIMENTS_CHAR_PATH, name + '.png'))
 
-def visualise_experiment(experiment_name):
-    confs, accuracy_avgs, time_avgs = get_experiment_results(experiment_name)
-    visualise(accuracy_avgs, time_avgs, name=experiment_name, labels=confs)
 
-def visualise_experiment_configuration(experiment_name, conf):
+def visualise_experiment(experiment_name, save=False, max=True):
+    confs, accuracy_avgs, time_avgs = get_experiment_results(experiment_name, max)
+    visualise(accuracy_avgs, time_avgs, name=experiment_name, labels=confs, save=save)
+
+
+def visualise_experiment_configuration(experiment_name, conf, save=False):
     accuracies, times = get_configuration_results(experiment_name, conf)
-    visualise(accuracies, times)
+    visualise(accuracies, times, save=save, name='_'.join([experiment_name, conf]))
 
 
-
-visualise_experiment_configuration("keepprob", "0.9")
+for exp in experiments:
+    visualise_experiment(exp, max=True, save=True)
+# visualise_experiment_configuration("preprocess", conf="all", save=True)
