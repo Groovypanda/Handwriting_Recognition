d01e53c82573719d92ba67c63f12ef19d00abb9f
raddecke <ruben.dedecker@ugent.be>
2017-12-11 01:34:37 +0100

    undone padding, not needed, was another bug

--- src/word_normalizer.py
+++ src/word_normalizer.py
@@ -6,16 +6,16 @@ PADDING = 5
 def normalize_word(word_image):
 
     height, width = word_image.shape[:2]
-    
+
 
     difference_percentage = NORMALIZED_HEIGHT / height
     normalized_width = width * difference_percentage
 
     normalized_word = cv2.resize(word_image, (int(normalized_width), NORMALIZED_HEIGHT))
 
-    norm_height, norm_width = normalized_word.shape[:2]
+    #norm_height, norm_width = normalized_word.shape[:2]
 
-    container = cv2.bitwise_not(np.zeros((norm_height + (2*PADDING), norm_width + (2*PADDING)), np.uint8))
-    container[PADDING:norm_height+PADDING ,PADDING:norm_width+PADDING] = normalized_word
+    #container = cv2.bitwise_not(np.zeros((norm_height + (2*PADDING), norm_width + (2*PADDING)), np.uint8))
+    #container[PADDING:norm_height+PADDING ,PADDING:norm_width+PADDING] = normalized_word
 
-    return container
+    return normalized_word
